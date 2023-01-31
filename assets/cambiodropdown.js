e7405fe7a9660881702a94c6aac80a0abe45f227
Juan Marcos Gonzalez <gonzalezromojm@hotmail.com>
2023-01-30 23:42:16 -0600

    Tercer opcion

--- assets/cambiodropdown.js
+++ assets/cambiodropdown.js
@@ -1,27 +1,48 @@
 
 const changeDropdown = () => {
 
-    const dropdown = document.getElementById("Dropdowncategories");
-    const dropdownCategories = `                        
-    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
-    aria-expanded="false">
-    Categorias
+    const navstyle = document.getElementById("navbarScroll");
+    const buttonstyle = `                        
+    <a class="nav-link active" href="#"><button class="btn">Inicio</button></a>
+    <div class="nav-item dropdown" id="Dropdowncategories">
+        <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            <!-- <img src="assets//images/microsoft.svg" alt="ventana"> -->
+            Categorias
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Playeras Caballero</a></li>
+            <li><a class="dropdown-item" href="#">Playeras Dama</a></li>
+            <li><a class="dropdown-item" href="#">Sudaderas</a></li>
+        </ul>
+    </div>
+
+    <a class="nav-link " href="#"><button class="btn"> Acceder</button></a>
+    <a class="nav-link " href="#"><button class="btn">Contáctanos</button></a>
+    <a class="nav-link " href="#"><button class="btn">Conócenos</button></a>
+    <a class="nav-link nav-item active" href="#">
+    <button class="btn ">
+        <img src="assets/images/cart2.svg" alt="carro-de-la-compra">
     </button>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Playeras Caballero</a></li>
-        <li><a class="dropdown-item" href="#">Playeras Dama</a></li>
-        <li><a class="dropdown-item" href="#">Sudaderas</a></li>
-    </ul>`;
-    const buttonsCategories= `
-    <a class="nav-link " href="#"><button class="btn">Playeras de Caballero</button></a>
-    <a class="nav-link " href="#"><button class="btn">Playeras de dama</button></a>
-    <a class="nav-link " href="#"><button class="btn">Sudaderas</button></a>
+    </a>`;
+    const listStyle= `
+    <a class="nav-link liststyle" href="#">Inicio</a>
+    <a class="nav-link liststyle" href="#">Playeras de Caballero</a>
+    <a class="nav-link liststyle" href="#">Playeras de dama</a>
+    <a class="nav-link liststyle" href="#">Sudaderas</a>
+    <a class="nav-link liststyle" href="#">Acceder</a>
+    <a class="nav-link liststyle" href="#">Contáctanos</a>
+    <a class="nav-link liststyle" href="#">Conócenos</a>
+    <a class="nav-link nav-item active" href="#">
+        <img src="assets/images/cart2.svg" alt="carro-de-la-compra">
+    </a>
     `;
+
     let ventana = window.innerWidth;
-    ventana<768? dropdown.innerHTML = buttonsCategories:dropdown.innerHTML = dropdownCategories;
+    ventana<768? navstyle.innerHTML = listStyle:navstyle.innerHTML = buttonstyle;
     window.addEventListener('resize', () => {  
         ventana = window.innerWidth;
-        ventana<768? dropdown.innerHTML = buttonsCategories:dropdown.innerHTML = dropdownCategories;
+        ventana<768? navstyle.innerHTML = listStyle:navstyle.innerHTML = buttonstyle;
     });
 }
 
