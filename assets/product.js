1890e492da8f00762fb07e137151d95ef3d34541
roubnsz <rubenmickj@gmail.com>
2023-02-04 22:01:14 -0600

    cambios css

--- assets/product.js
+++ assets/product.js
@@ -1,8 +1,8 @@
 function addItem(item) {
 
     const itemTemplate = `
-    <div id="masVendido1"class="col-6 col-md-3  p-2  descripcion ">
-    <img id="imgMasVendido1" class="avatares img-fluid" src="${item.img}" alt="image">
+    <div id="masVendido1"class="col-6 col-md-3 col-sm-4 p-2  descripcion ">
+    <img class="avatares img-fluid" src="${item.img}" alt="image">
     <p id="nombreMasVendido1">${item.name}</p>
     <div class="row align-items-center d-flex justify-content-center">
     <div id="precioMasVendido1"class=" col-8 col-md-4 bg-primary p-2  descripcion ">${item.price}$</div>
@@ -14,17 +14,17 @@ function addItem(item) {
     itemsContainer.innerHTML += itemTemplate;
   }
 
-  addItem({'name':'El proximo rey de los piratas',
+  addItem({'name':'El proximo rey',
     'img':'https://i.ibb.co/28jHTYH/luffy-pld-mar.jpg',
     'price':'40'})
   
-    addItem({'name':'El proximo rey de los piratas',
+    addItem({'name':'El proximo rey',
     'img':'https://i.ibb.co/M5NvsKv/luffi-pl-neg.jpg',
     'price':'40'})
-    addItem({'name':'ALll Might, Plus Ultra, Se tambien un heroe',
+    addItem({'name':'Se tambien un heroe',
     'img':'https://i.ibb.co/JmcQ3Xb/deku-pld-neg.jpg',
     'price':'40'})
-    addItem({'name':'anime',
+    addItem({'name':'anime dfgdfghdfgg',
     'img':'https://i.ibb.co/5LR2LWs/tanjiro-pld-neg.jpg',
     'price':'40'})
     addItem({'name':'Tayto',
