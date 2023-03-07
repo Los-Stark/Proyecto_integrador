1ba2eb2ff67b21a7345f5a7611dcd2ff4516744e
Juan Marcos Gonzalez <gonzalezromojm@hotmail.com>
2023-03-07 01:29:53 -0600

    Cambios para tener el inicio con base de datos

--- assets/js/products.js
+++ assets/js/products.js
@@ -2,35 +2,56 @@
  * 
  * @param {*} item Cada objeto que se puede colocar para vender y su formato
  */
-cont =1;
+cont = 1;
 function addItem(item) {
 
-    const itemTemplate = `
-    <div id="CardProduct"class="col-6 col-md-3 col-sm-4 p-2  mx-1 my-1">
-    <img class="productsImage img-fluid" src="${item.img}" alt="image">
-    <p id="CardName">${item.name}</p>
+  const itemTemplate = `
+    <div id="CardProduct"class="col-6 col-md-3 col-sm-4 p-2  mx-2 my-1">
+    <img class="productsImage img-fluid" src="${item.imageURL}" alt="image">
+    <p id="CardName" >${item.name}</p>
     <div class="row align-items-center d-flex justify-content-center">
     <div id="CardPrice"class=" col-8 col-md-4 p-2  ">$${item.price}</div>
     <div class="size">
-                        <p>
-                        <input class ="clothesSizes clothesSizesCH" id="clothesSizesCH${cont}" name="typeSize${cont}" type="radio" value="CH">
-                        <label for="clothesSizesCH${cont}" class="col-sm-3">  CH  </label>
-                        <input class ="clothesSizes clothesSizesM" id="clothesSizesM${cont}" name="typeSize${cont}" type="radio" value="M" checked>
-                        <label for="clothesSizesM${cont}" class="col-sm-3">  M  </label>
-                        <input class ="clothesSizes clothesSizesG" id="clothesSizesG${cont}" name="typeSize${cont}" type="radio" value="G">
-                        <label for="clothesSizesG${cont}" class="col-sm-3">  G  </label>
-                        </p>
+                                <p>
+                                 <input class ="clothesSizes clothesSizesCH" id="clothesSizesCH${cont}" name="typeSize${cont}" type="radio" value="CH">
+                                 <label for="clothesSizesCH${cont}" class="col-sm-3">  CH  </label>
+                                 <input class ="clothesSizes clothesSizesM" id="clothesSizesM${cont}" name="typeSize${cont}" type="radio" value="M" checked>
+                                 <label for="clothesSizesM${cont}" class="col-sm-3">  M  </label>
+                                 <input class ="clothesSizes clothesSizesG" id="clothesSizesG${cont}" name="typeSize${cont}" type="radio" value="G">
+                                 <label for="clothesSizesG${cont}" class="col-sm-3">  G  </label>
+                                 </p>
                     </div>
     <button class="add-to-cart">Agregar al carrito</button>
         </div>
       </div>
     `;
 
-    const itemsContainer = document.getElementById("section");
-    itemsContainer.innerHTML += itemTemplate;
-    cont++;
-  }
+  const itemsContainer = document.getElementById("section");
+  itemsContainer.innerHTML += itemTemplate;
+  cont++;
+}
 
+
+async function getProducts() {
+  const url = `https://backendproyecto-production.up.railway.app/api/products`;
+
+  await fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(products => {
+      addItem(products[7]);
+      addItem(products[3]);
+      addItem(products[12]);
+      addItem(products[20]);
+      addItem(products[24]);
+      addItem(products[15]);
+    })
+    .catch(error => console.log("Error " + error));
+    getAllButtons();
+}
+getProducts();
+/* 
     addItem({'name':'Izuku Midoriya',
     'img':'./assets/images/woman-t-shirt/deku-pld-mar.png',
     'price':150})
@@ -45,5 +66,5 @@ function addItem(item) {
     'price':150})
     addItem({'name':'Tanjiro Kamado',
     'img':'./assets/images/woman-t-shirt/tanjiro-pld-mar.png',
-    'price':150})
+    'price':150}) */
   
